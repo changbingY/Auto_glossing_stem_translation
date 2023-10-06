ec67b22a0b37e9a0e391b8b35ba1cafe3fc8d74a
Changbing Yang <58204931+changbingY@users.noreply.github.com>
2023-10-06 13:01:04 -0700

    Update containers.py

--- containers.py
+++ containers.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
 GlossingFileData = namedtuple(
-    "GlossingFileData", field_names=["sources", "targets", "morphemes"]
+    "GlossingFileData", field_names=["sources", "targets", "morphemes","translations"]
 )
 Batch = namedtuple(
     "Batch",
@@ -13,6 +13,11 @@ Batch = namedtuple(
         "word_batch_mapping",
         "word_targets",
         "word_target_lengths",
+        "trans_sentences",
+        "trans_sentence_lengths",
+        "trans_word_lengths",
+        "trans_word_extraction_index",
+        "trans_word_batch_mapping",
         "morpheme_extraction_index",
         "morpheme_lengths",
         "morpheme_word_mapping",

99d917605ab169d62f18037b73e7be9a2ee99378
Changbing Yang <58204931+changbingY@users.noreply.github.com>
2023-11-24 11:01:00 -0800

    Add files via upload

---
+++ decoder/train.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+  
+#SBATCH --account=def-msilfver
+#SBATCH --time 20:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=50G
+#SBATCH --mail-user=changbing.yang@colorado.edu
+#SBATCH --job-name=train_fairseq_5Lang
+#SBATCH --nodes=1   
+
+python main.py --language Gitksan  --model morph --track 1

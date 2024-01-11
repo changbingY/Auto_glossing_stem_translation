fbf03ea081c6de474e27f2e57b869928f7635c52
Changbing Yang <58204931+changbingY@users.noreply.github.com>
2024-01-10 23:24:00 -0800

    Add files via upload

---
+++ bert_decoder_character/tuning.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+  
+#SBATCH --account=def-msilfver
+#SBATCH --time 20:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=50G
+#SBATCH --mail-user=changbing.yang@colorado.edu
+#SBATCH --job-name=train_fairseq_5Lang
+#SBATCH --nodes=1   
+
+python hyperparameter_tuning.py \
+    --language Lezgi \
+    --model morph \
+    --track 1 \
+    --trials 1

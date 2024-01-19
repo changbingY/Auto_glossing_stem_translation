5cbae9f77b0e2ba6bf153009e48b6a82cb220a36
Changbing Yang <58204931+changbingY@users.noreply.github.com>
2024-01-19 14:01:12 -0800

    Update bert.py

--- bert_decoder_character/bert.py
+++ bert_decoder_character/bert.py
@@ -11,12 +11,12 @@ class BERTBasedEncoder(nn.Module):
         super(BERTBasedEncoder, self).__init__()
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         self.bert = BertModel.from_pretrained('bert-base-uncased').to(device)
-        self.reduce_dim = nn.Linear(768, projection_dim).to(device)
+        #self.reduce_dim = nn.Linear(768, projection_dim).to(device)
     
     def forward(self, inputs: List[str], lengths: torch.Tensor) -> torch.Tensor:
         with torch.no_grad():
             encoded_input = self.tokenizer(inputs, padding=True, return_tensors="pt").to(device)
             outputs = self.bert(**encoded_input)
             last_hidden_states = outputs.last_hidden_state  # Extract the last hidden state
-        last_hidden_states = self.reduce_dim(last_hidden_states).to(device)
+        #last_hidden_states = self.reduce_dim(last_hidden_states).to(device)
         return last_hidden_states.to(device)  # You may need to do further processing based on the specific requirements of your model

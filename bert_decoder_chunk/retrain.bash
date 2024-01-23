62cc3e42db78c3ebd3766ba86687cb3a4e7340dd
Changbing Yang <58204931+changbingY@users.noreply.github.com>
2024-01-22 17:07:53 -0800

    Update retrain.bash

--- bert_decoder_chunk/retrain.bash
+++ bert_decoder_chunk/retrain.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
   
 #SBATCH --account=def-msilfver
-#SBATCH --time 200:00:00
+#SBATCH --time 20:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=120G
 #SBATCH --mail-user=changbing.yang@colorado.edu
 #SBATCH --job-name=train_fairseq_5Lang
 #SBATCH --nodes=1   
 
-python  retrain_best_hyperparameters.py --language Arapaho --model morph --track 1 --trial 10
+python  retrain_best_hyperparameters.py --language Tsez --model morph --track 1 --trial 1
